66fae2c8791534cf0a9cd74886d872f7449f4ca8
evilninja6726 <59741387+evilninja6726@users.noreply.github.com>
2021-02-22 02:42:54 +0530

    Add files via upload
    Problem based on Children Sum Property, we have to check if the root is equals to sum of right and left child if yes returns true else returns false.
    Time - O(n)
    Space - O(1)

---
+++ ChildrenSumProperty.java
@@ -0,0 +1,25 @@
+package treeDs;
+
+public class ChildrenSumProperty {
+    public static void main(String[] args) {
+        Node root = new Node(20);
+        root.left = new Node(8);
+        root.left.left = new Node(4);
+        root.left.right = new Node(4);
+        root.right = new Node(11);
+        System.out.println(childrenSumProperty(root));
+    }
+
+    public static boolean childrenSumProperty(Node root) {
+        if (root.left == null && root.right == null)
+            return true;
+        if (root.left.data + root.right.data == root.data)
+            return true;
+        int sum = 0;
+        if (root.left != null)
+            sum += root.left.data;
+        if (root.right != null)
+            sum += root.right.data;
+        return root.data == sum && childrenSumProperty(root.left) && childrenSumProperty(root.right);
+    }
+}

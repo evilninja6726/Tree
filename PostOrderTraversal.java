1515a82fbf0e883fae49cb7fefb7cebc86b8ccfa
evilninja6726 <59741387+evilninja6726@users.noreply.github.com>
2021-02-21 22:21:09 +0530

    Add files via upload
    Postorder Traversal of a Tree in O(n) Time and O(h) Space where h is the maximum height of Tree.

---
+++ PostOrderTraversal.java
@@ -0,0 +1,23 @@
+package treeDs;
+
+public class PostOrderTraversal {
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.left.left = new Node(40);
+        root.left.right = new Node(50);
+        root.left.right.left = new Node(70);
+        root.left.right.right = new Node(80);
+        root.right = new Node(30);
+        root.right.right = new Node(60);
+        postOrderTraversal(root);
+    }
+
+    public static void postOrderTraversal(Node root) {
+        if (root != null) {
+            postOrderTraversal(root.left);
+            postOrderTraversal(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+}

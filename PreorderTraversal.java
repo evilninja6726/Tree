b8a477eb99f90551c3f946d9eadecfc9b0d3bc11
evilninja6726 <59741387+evilninja6726@users.noreply.github.com>
2021-02-21 22:17:57 +0530

    Add files via upload
    Preorder Traversal of Tree in O(n) Time and O(h) Space where h is maximum height of the Tree.

---
+++ PreorderTraversal.java
@@ -0,0 +1,23 @@
+package treeDs;
+
+public class PreorderTraversal {
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.left.left = new Node(40);
+        root.left.right = new Node(50);
+        root.left.right.left = new Node(70);
+        root.left.right.right = new Node(80);
+        root.right = new Node(30);
+        root.right.right = new Node(60);
+        preorderTraversal(root);
+    }
+
+    public static void preorderTraversal(Node root) {
+        if (root != null) {
+            System.out.print(root.data + " ");
+            preorderTraversal(root.left);
+            preorderTraversal(root.right);
+        }
+    }
+}
